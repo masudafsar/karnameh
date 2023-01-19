9f13d7774523426ae01b0fe7f357ac3128f9716f
Masud Afsar <masud.afsar@gmail.com>
2023-01-19 18:31:10 +0330

    Create base of router

---
+++ src/routes/router.tsx
@@ -0,0 +1,11 @@
+import { mainRouter } from '@routes/mainRouter';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routers = createBrowserRouter([...mainRouter]);
+
+function Router() {
+  return <RouterProvider router={routers} />;
+}
+
+export default Router;

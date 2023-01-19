b3594df1f232e1d529101bc516f8af2c3ffee798
Masud Afsar <masud.afsar@gmail.com>
2023-01-20 00:48:12 +0330

    Implement user menu and avatar components

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -1,4 +1,5 @@
 import Button from '@component/button';
+import UserMenu from '@component/userMenu';
 import plusIcon from '@iconify/icons-uil/plus';
 import { Icon } from '@iconify/react';
 import React from 'react';
@@ -7,7 +8,7 @@ interface IProps {}
 
 function AppBar({}: IProps) {
   return (
-    <header className="bg-white p-4">
+    <header className="bg-white px-4 py-3">
       <div className="container mx-auto flex justify-between items-center">
         <div>
           <h1 className="text-h1 font-bolder">لیست سوالات</h1>
@@ -16,6 +17,13 @@ function AppBar({}: IProps) {
           <Button onClick={() => {}} prefix={<Icon icon={plusIcon} />}>
             سوال جدید
           </Button>
+          <UserMenu
+            user={{
+              id: '5eee587a-f00c-4541-99bf-dbbeff63f276',
+              fullName: 'مسعود افسر',
+              avatar: '/avatars/5eee587a-f00c-4541-99bf-dbbeff63f276.png',
+            }}
+          />
         </div>
       </div>
     </header>
---
+++ src/types/user.type.ts
@@ -0,0 +1,5 @@
+export interface IUserType {
+  id: string;
+  fullName: string;
+  avatar: string;
+}

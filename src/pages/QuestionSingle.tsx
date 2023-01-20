6918b05557de1beee3a03098fd021dd5aa88c84b
Masud Afsar <masud.afsar@gmail.com>
2023-01-20 14:16:42 +0330

    Implement reply form

--- src/pages/QuestionSingle.tsx
+++ src/pages/QuestionSingle.tsx
@@ -1,4 +1,5 @@
 import AnswerCard from '@component/answerCard';
+import Button from '@component/button';
 import QuestionCard from '@component/questionCard';
 import db from 'db.json';
 import React from 'react';
@@ -23,6 +24,28 @@ function QuestionSingle({}: IProps) {
           </div>
         </>
       )}
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert('SUBMITTED');
+        }}
+      >
+        <h3 className="text-h1 font-bolder mt-5 mb-4">پاسخ خود را ثبت کنید</h3>
+        <div className="mb-8">
+          <label className="flex flex-col">
+            <span className="text-sm font-bold mb-2">پاسخ خود را بنویسید</span>
+            <textarea
+              rows={8}
+              placeholder="متن پاسخ"
+              className="py-3 px-4 mb-2 resize-y bg-white border border-gray-light rounded-sm"
+            />
+          </label>
+          <p className="text-sm text-error">متن پاسخ الزامی می‌باشد</p>
+        </div>
+        <Button variant="contained" type="submit">
+          ارسال پاسخ
+        </Button>
+      </form>
     </>
   );
 }
